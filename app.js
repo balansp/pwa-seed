72f3a8e04097074ff7bf67e69c4c4c296b2dca23
Balan <balan.sp@sap.com>
2020-05-01 00:00:31 +0530

    Calc implemented

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+  (function(){
 
+      function $(selector) {
+        return document.querySelector(selector);
+      }
+
+       //Events
+      $(".btn").addEventListener("click", function(event) {
+        $('#discount').value = "";
+        $('#afterDiscount').value = "";
+        let discount = 0;
+        let amount = $('#amount').value;
+        let percentage = $('#percentage').value;
+        if(amount && percentage){
+            discount = (amount * percentage) / 100;
+            $('#discount').value = discount;
+            $('#afterDiscount').value = amount - discount;
+        } 
+      });
  
-  window.addEventListener('load', async e => {
-    if ('serviceWorker' in navigator) { 
-        try {
-            navigator.serviceWorker.register('./sw.js'); 
+      window.addEventListener('load', async e => {
+        if ('serviceWorker' in navigator) {
+          try {
+            navigator.serviceWorker.register('./sw.js');
             console.log("Service Worker register")
-        } catch (e) {
-          alert('ServiceWorker registration failed. Sorry about that.'); 
+          } catch (e) {
+            alert('ServiceWorker registration failed. Sorry about that.');
+          }
         }
-      }  
+      });
 
-  });
+    })();
